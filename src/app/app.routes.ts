b4e9c951e418726faa5e43c03b609c3865498fb0
Nicolas Munar <nic98mr@gmail.com>
2025-12-30 12:42:17 -0500

    Adicion de paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FormularioProducto } from './listado-productos/formulario-producto/formulario-producto';
+import { Producto } from './listado-productos/producto/producto';
+import { ListadoProductos } from './listado-productos/listado-productos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: ListadoProductos}, //localhost:4200/
+    {path:'formulario', component: ListadoProductos}, //localhost:4200/listado
+    {path:'agregar', component: FormularioProducto},//localhost:4200/producto
+    {path:'editar/:id', component: FormularioProducto},//localhost:4200/producto   
+];
